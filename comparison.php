9e9c88c2637ce45687fce81a0288a12cefb2077e
Zayne <star1582@naver.com>
2024-03-23 13:34:34 +0000

    comparison

---
+++ comparison.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Comparison Operators &amp; Boolean data type</h1>
+    <h2>1==1</h2>
+    <?php
+    var_dump(1==1);  
+    ?>
+    <h2>1>1</h2>
+    <?php 
+    var_dump(1>1);
+    ?>
+</body>
+
+</html>
