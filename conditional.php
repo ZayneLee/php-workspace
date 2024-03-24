bbe55c65487a28b4b774c41924d43ab38d965a1d
Zayne <star1582@naver.com>
2024-03-24 19:56:08 +0000

    conditional

---
+++ conditional.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Conditional</h1>
+    <h2>if</h2>
+    <?php 
+      echo '1<br>';  
+      if(false) {
+          echo '2-1<br>';  
+      } else {
+        echo '2-2<br>';
+      }
+      echo '3<br>';  
+    ?>
+</body>
+
+</html>
